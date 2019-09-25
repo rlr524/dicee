46679eae98705d32e6baa8d48d6dc681f145f191
Rob Ranf <robranf@gmail.com>
2019-09-25 14:28:57 -0700

    Added die and button outlets

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageViewLeft: UIImageView!
+    @IBOutlet weak var diceImageViewRight: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated
+    }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+    }
+    
 }
 
